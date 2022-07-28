a80f1b7bfe9262ae35faf7736057c43e250e3e05
Reza Javadi <rezaj@LAPTOP-C9N1K6SL>
2022-07-28 17:48:18 +0430

    add coulmn c

--- Tables/dbo.test.sql
+++ Tables/dbo.test.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[test] (
   [a] [varchar](50) NULL,
   [b] [varchar](50) NULL,
-  [c] [varchar](50) NULL
+  [c] [varchar](50) NULL,
+  [d] [varchar](50) NULL
 )
 ON [PRIMARY]
 GO
