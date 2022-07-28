2f1b5994c54bdf039914fc944692975d6f16bb20
Reza Javadi <rezaj@LAPTOP-C9N1K6SL>
2022-07-28 17:18:50 +0430

    firtst commit

---
+++ Tables/dbo.StudentAddresses.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[StudentAddresses] (
+  [StudentAddressId] [int] IDENTITY,
+  [Address] [nvarchar](max) NOT NULL,
+  [City] [nvarchar](max) NOT NULL,
+  [State] [nvarchar](max) NOT NULL,
+  [Country] [nvarchar](max) NOT NULL,
+  [AddressOfStudentId] [int] NOT NULL,
+  CONSTRAINT [PK_StudentAddresses] PRIMARY KEY CLUSTERED ([StudentAddressId])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [IX_StudentAddresses_AddressOfStudentId]
+  ON [dbo].[StudentAddresses] ([AddressOfStudentId])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[StudentAddresses]
+  ADD CONSTRAINT [FK_StudentAddresses_Students_AddressOfStudentId] FOREIGN KEY ([AddressOfStudentId]) REFERENCES [dbo].[Students] ([Id]) ON DELETE CASCADE
+GO
---
+++ Tables/dbo.Students.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students] (
+  [Id] [int] IDENTITY,
+  [Name] [nvarchar](max) NOT NULL,
+  CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
